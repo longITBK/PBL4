2fd38f573061ac7fe54ef5b3d4e4072207e21eab
LONG HOANG <hoangnhatbaolong0215971@gmail.com>
2022-12-22 14:18:56 +0700

    PBL4_Project

---
+++ Forms/nhap.py
@@ -0,0 +1,5 @@
+import subprocess
+from os.path import abspath
+a = 15
+print(round(a, 0))
+print(a)
---
+++ StyleSheet/Style.py
@@ -0,0 +1,29 @@
+class Style():
+    def Style_Btn_On(self):
+        return """border-width: 2px;
+                    border-radius: 10px;
+                    border-color: beige;
+                    border: 2px solid black;
+                    background-color: green"""
+    
+    def Style_Btn_Off(self):
+        return """border-width: 2px;
+                    border-radius: 10px;
+                    border-color: beige;
+                    border: 2px solid black;
+                    background-color: grey"""
+
+    def Style_Btn_AM(self):
+        return """border-width: 2px;
+                    border-radius: 10px;
+                    border-color: beige;
+                    border: 2px solid grey;
+                    background-color: rgb(153, 193, 241);"""
+
+    def Style_Btn_PM(self):
+        return """border-width: 2px;
+                    border-radius: 10px;
+                    border-color: beige;
+                    border: 2px solid grey;
+                    background-color: orange;"""
+
